5cbd11738e57bf568c007d0be0cfb45464344f3f
Tosha <pateltk245.ce@gmail.com>
2023-12-18 23:14:34 +0530

    backend

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux"; 
+    
+const PrivateRoute = () => {
+    const { userInfo } = use
+    return (
+        <div>PrivateRoute</div>
+    )
+}
+
+export default PrivateRoute
