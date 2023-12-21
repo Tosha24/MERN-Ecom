226dc931520408dea34bfc329a01c5e0a963fd31
Tosha <pateltk245.ce@gmail.com>
2023-12-21 22:12:57 +0530

    category

---
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -0,0 +1,137 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import {
+  useCreateCategoryMutation,
+  useUpdateCategoryMutation,
+  useDeleteCategoryMutation,
+  useFetchCategoriesQuery,
+} from "../../redux/api/categoryApiSlice";
+import CategoryForm from "../../components/CategoryForm.jsx";
+import Modal from "../../components/Modal.jsx";
+
+const CategoryList = () => {
+  const [name, setName] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [updateName, setUpdateName] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  // generate methods from mutations and queries
+  const { data: categories } = useFetchCategoriesQuery();
+  const [createCategory] = useCreateCategoryMutation();
+  const [updateCategory] = useUpdateCategoryMutation();
+  const [deleteCategory] = useDeleteCategoryMutation();
+
+  // functions
+  const handleCreateCategory = async (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      toast.error("Category name is required");
+      return;
+    }
+
+    try {
+      const result = await createCategory({ name }).unwrap();
+
+      if (result.error) {
+        toast.error(result.error);
+      } else {
+        setName("");
+        toast.success(`${result.name} is created`);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Creating category failed, try again");
+    }
+  };
+
+  const handleUpdateCategory = async (e) => {
+    e.preventDefault();
+
+    if (!updateName) {
+      toast.error("Category name is required");
+      return;
+    }
+
+    try {
+      const result = await updateCategory({
+        categoryId: selectedCategory._id,
+        updatedCategory: { name: updateName },
+      }).unwrap();
+
+      if (result.error) {
+        toast.error(result.error);
+      } else {
+        toast.success(`${result.name} is updated`);
+        selectedCategory(null);
+        setUpdateName('');
+        setModalVisible(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDeleteCategory = async (e) => {
+    try {
+      const result = await deleteCategory(selectedCategory._id).unwrap();
+      if (result.error) {
+        toast.error(result.error);
+      } else {
+        toast.success(`${result.name} is deleted`);
+        setSelectedCategory(null);
+        setModalVisible(false);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Category deletion failed. try again");
+    }
+  };
+
+  return (
+    <div className="ml-[10rem] flex flex-col md:flex-row">
+      {/* <AdminMenu /> */}
+      <div className="md:w-3/4 p-3">
+        <div className="h-12">Manage Categories</div>
+        <CategoryForm
+          value={name}
+          setValue={setName}
+          handleSubmit={handleCreateCategory}
+        />
+        <br />
+        <hr />
+
+        <div className="flex flex-wrap">
+          {categories?.map((category) => (
+            <div key={category._id}>
+              <button
+                className="bg-white border border-pink-500 text-pink-500 py-2 px-4 rounded-lg m-3 hover:bg-pink-500 hover:text-white focus:outline-none foucs:ring-2 focus:ring-pink-500 focus:ring-opacity-50"
+                onClick={() => {
+                  {
+                    setModalVisible(true);
+                    setSelectedCategory(category);
+                    setUpdateName(category.name);
+                  }
+                }}
+              >
+                {category.name}
+              </button>
+            </div>
+          ))}
+        </div>
+
+        <Modal isOpen={modalVisible} onClose={() => setModalVisible(false)}>
+          <CategoryForm
+            value={updateName}
+            setValue={(value) => setUpdateName(value)}
+            handleSubmit={handleUpdateCategory}
+            buttonText="Update"
+            handleDelete={handleDeleteCategory}
+          />
+        </Modal>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
--- frontend/src/pages/Admin/UserList.jsx
+++ frontend/src/pages/Admin/UserList.jsx
@@ -72,7 +72,7 @@ const UserList = () => {
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-semibold mb-4">Users</h1>
+      <h1 className="text-3xl font-semibold mb-10 flex items-center justify-center underline">USERS</h1>
       {isLoading ? (
         <Loader />
       ) : error ? (
@@ -82,9 +82,9 @@ const UserList = () => {
       ) : (
         <div className="flex flex-col md:flex-row">
           {/* <AdminMenu /> */}
-          <table className="w-full md:w-4/5 mx-auto">
+          <table className="w-full md:w-4/5 mx-auto border">
             <thead>
-              <tr>
+              <tr className='border'>
                 <th className="px-4 py-2 text-left">ID</th>
                 <th className="px-4 py-2 text-left">NAME</th>
                 <th className="px-4 py-2 text-left">EMAIL</th>
